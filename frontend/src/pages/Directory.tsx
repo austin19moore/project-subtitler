93de607dcce1925c8c42085a2923facde766df1e
Austin Moore <austin19moore@gmail.com>
2026-08-28 21:53:07 -0500

    Fix directory grid alignment when there is less than 3 cards

--- frontend/src/pages/Directory.tsx
+++ frontend/src/pages/Directory.tsx
@@ -5,10 +5,10 @@ const iconsBaseUrl = `${import.meta.env.BASE_URL}icons/`;
 export const Directory = () => {
     return (
         <div className="flex flex-col items-center py-8 md:py-12">
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12 max-w-4xl w-full">
+            <div className="flex flex-wrap justify-center gap-12 max-w-4xl w-full">
                 {streamers.map((streamer) => (
                     <Link
-                        className="flex bg-neutral-800 rounded-lg transition px-6 py-4 gap-4 hover:-translate-y-0.5 hover:bg-neutral-700 focus-visible:ring-2 focus-visible:ring-neutral-400"
+                        className="flex bg-neutral-800 rounded-lg transition px-6 py-4 gap-4 hover:-translate-y-0.5 hover:bg-neutral-700 focus-visible:ring-2 focus-visible:ring-neutral-400 w-72"
                         to={`/${streamer.slug}`}
                         key={streamer.slug}
                     >
